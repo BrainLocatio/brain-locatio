ccd6f718c8df1c7a8806b9b88d14ef4d5ebb48f4
Nikoleta Kocúrová <nikoletakocurova@Nikoletas-MacBook-Air.local>
2025-05-08 08:07:15 +0200

    refactor(dsa/001_two_sum): Simplify solution

--- data-structures-and-algorithms/001_two_sum/solution.py
+++ data-structures-and-algorithms/001_two_sum/solution.py
@@ -1,6 +1,11 @@
 class Solution:
     def two_sum(self, nums: list[int], target: int) -> list[int]:
         """
+        Hash-map-based approach
+
+        The single-pass solution where indices of seen numbers are stored using a hash map,
+        while traversing once through the list.
+
         :param nums: list[int]: Numbers to search through for pairs that sum to the target
         :param target: int: The target sum to find in nums
         :return: list[int]: A list of two indices whose values add up to the target
@@ -8,22 +13,12 @@ class Solution:
         :example:
             two_sum([2, 7, 11, 15], 9) -> [0, 1]
         """
-        return self._find_two_sum_indices(target, nums)
-
-    @staticmethod
-    def _find_two_sum_indices(target: int, elements: list[int]) -> list[int]:
-        """
-        Hash-map-based approach
-
-        The single-pass solution where indices of seen numbers are stored using a hash map,
-        while traversing once through the list.
-        """
-        if len(elements) < 2:
+        if len(nums) < 2:
             return []
 
         num_to_index: dict[int, int] = {}
 
-        for idx, element in enumerate(elements):
+        for idx, element in enumerate(nums):
             complement: int = target - element
 
             if complement in num_to_index:
